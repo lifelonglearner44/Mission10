ddbd7c2fc34702686895df014b9a3922c0d326cf
Your Name <you@example.com>
2024-03-14 22:21:49 -0600

    updates

--- backend/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ backend/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a34a8c06b212369a8e20bbb2598c1f6f0ace2ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c2fd67dd5263df289d8e22300cf6137358e423f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
